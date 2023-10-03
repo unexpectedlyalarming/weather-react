cd1f1abc75151b7a2506ccf24e6f13120ae0e53a
antiblockchain <57572382+antiblockchain@users.noreply.github.com>
2023-10-03 15:22:33 -0500

    Fixed some weird issues

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,10 @@ function App() {
   const [input, setInput] = useState("")
   const [bgImg, setBgImg] = useState("")
   const [textColor, setTextColor] = useState("")
+  const [hasVisited, setHasVisited] = useState(false);
 
   async function handleSubmit (e) {
-    e.preventDefault;
+    e.preventDefault();
 
     fetch(
       `https://api.weatherapi.com/v1/current.json?key=ace4d94a1a8c4a129b5160532230504&q=${input.toString()}`,
@@ -45,24 +46,62 @@ function App() {
           query: response.location.name,
           page: 1,
           perPage: 1,
-        }).then((newResponse) => setBgImg(newResponse.response.results[0]))
-        console.log(bgImg)
-        //Bitshift operation to invert hexcode from API
-        let hex = bgImg.color.replace(/^#/, '');
-        hex = parseInt(hex, 16);
-        const invertHex = -hex & 0xFFFFFF;
-        setTextColor(`#${invertHex.toString(16)}`);
+        }).then((newResponse) => {
+          const newBgImg = newResponse.response.results[0];
+  
+          // Bitshift operation to invert hexcode from API
+        let hex = newBgImg.color.replace(/^#/, '');
+         hex = parseInt(hex, 16);
+          const invertHex = ~hex & 0xFFFFFF;
+          setTextColor(`#${invertHex.toString(16)}`);
+          setHasVisited(true);
+
+        // Update bgImg state
+        setBgImg(newBgImg);
+        })
+
 
 
 
       });
 
- 
+   
   }
-
-  return (
+  const dataContainer = (
+    <div className="data-container" style={{backgroundColor: bgImg.color}}>
+    <h1 id="location">{location}</h1>
+    <div className="condition-container">
+      <img src={imageURL} id="condition-img" />
+      <p id="weather">{condition}</p>
+    </div>
+    <p id="temp">{temp}</p>
+  </div>
+  )
+    if (hasVisited) return (
+      <>
+      <div className="container" style={{ backgroundImage: `url(${bgImg?.urls?.raw})`, color: textColor, textShadow: `2px 3px 2px black`} }>
+        <h2>Whats the weather like today?</h2>
+        <div className="input-container">
+          <form onSubmit={(e) => {e.preventDefault(); handleSubmit }}>
+            <input placeholder="Location" type="" id="input" onChange={(e) => setInput(e.target.value)}/>
+            <button id="submit" type="button" onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+  
+        <div className="data-container" style={{backgroundColor: bgImg.color}}>
+      <h1 id="location">{location}</h1>
+      <div className="condition-container">
+        <img src={imageURL} id="condition-img" />
+        <p id="weather">{condition}</p>
+      </div>
+      <p id="temp">{temp}</p>
+    </div>
+      </div>
+      </>
+    ) 
+    return (
     <>
-    <div className="container" style={{ backgroundImage: `url(${bgImg.urls.raw})`, color: textColor, textShadow: `2px 3px 2px ${bgImg.color}`} }>
+    <div className="container" style={{ backgroundImage: `url(${bgImg?.urls?.raw})`, color: textColor, textShadow: `2px 3px 2px black`} }>
       <h2>Whats the weather like today?</h2>
       <div className="input-container">
         <form onSubmit={(e) => {e.preventDefault(); handleSubmit }}>
@@ -70,14 +109,8 @@ function App() {
           <button id="submit" type="button" onClick={handleSubmit}>Submit</button>
         </form>
       </div>
-      <div className="data-container" style={{backgroundColor: bgImg.color}}>
-        <h1 id="location">{location}</h1>
-        <div className="condition-container">
-          <img src={imageURL} id="condition-img" />
-          <p id="weather">{condition}</p>
-        </div>
-        <p id="temp">{temp}</p>
-      </div>
+
+      
     </div>
     </>
   )
